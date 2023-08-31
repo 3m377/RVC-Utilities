bfba2d8c778b84fe627a7d789ec45196df9fbdfe
3m377 <99052784+3m377@users.noreply.github.com>
2023-08-31 05:37:35 -0700

    Update installer.py

--- installer.py
+++ installer.py
@@ -32,8 +32,8 @@ print('Please move all files from the Mangio-RVC-Fork folder to the RVC-beta fol
 print('It will tell you "The destination has X files with the same names",')
 print('Just click "Replace the files in the destination".')
 print('DO NOT CLOSE THIS WINDOW, THE INSTALLATION WILL END IF YOU DO')
-print('Once you\'re done copying all of the files, delete the Mangio-RVC-Fork folder and return to this window.')
-input('Press Enter to continue...')
+print('Once you\'re done copying all of the files, delete the Applio-RVC-Fork folder and return to this window.')
+os.system('pause')
 
 # Install python requirements
 os.chdir('RVC-beta')
@@ -63,4 +63,4 @@ os.chdir('..')
 os.remove('7za.exe')
 
 print('Successfully install RVC-beta and EasyGUI')
-input('Press Enter to continue...')
+os.system('pause')
