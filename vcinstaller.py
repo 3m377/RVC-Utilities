38ba8373bed5e60117f2fb54b1e3b020c59e65e2
3m377 <99052784+3m377@users.noreply.github.com>
2023-08-31 23:42:52 -0700

    Update vcinstaller.py

--- vcinstaller.py
+++ vcinstaller.py
@@ -6,6 +6,14 @@ vcVersion = "1.5.3.13"
 vcFileName = "MMVCServerSIO_win_onnxgpu-cuda_v.1.5.3.13.zip"
 vcFileURL = "https://huggingface.co/wok000/vcclient000/resolve/main/MMVCServerSIO_win_onnxgpu-cuda_v.1.5.3.13.zip"
 
+# If user has updated version, use it instead
+input("Do you have an updated version of the voice changer? (y/n): ")
+if input == "y": 
+    vcVersion = "User Specified"
+    vcFileName = "MMVCServerSIO_userspecified.zip"
+    print("EXAMPLE LINK: https://huggingface.co/wok000/vcclient000/resolve/main/MMVCServerSIO_win_onnxgpu-cuda_v.1.5.3.13.zip")
+    vcFileURL = input("Please input the direct download link to the updated version: ")
+
 os.system("cls")
 print("WARNING: This may not be updated, it depends if I remember to update the script.")
 print(f"Current version: {vcVersion}")
